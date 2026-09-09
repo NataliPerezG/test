497a40d48e78bc0ccd172200c53e522a05a969f3
NataliPerezG <nataliperezgarcia311@gmail.com>
2026-09-09 09:30:46 -0500

    Updates

---
+++ modules/questions.js
@@ -0,0 +1,47 @@
+export const questions = [
+    {
+        question: "Cuál es la capital de Francia?",
+        answers: [
+            { text: 'londres', correct: false },
+            { text: 'Paris', correct: true },
+            { text: 'Berlin', correct: false },
+            { text: 'Madrid', correct: false },
+        ]
+    },
+    {
+        question: "¿Cuál es el océano más grande del mundo?",
+        answers: [
+            { text: 'Atlántico', correct: false },
+            { text: 'Índico', correct: false },
+            { text: 'Pacífico', correct: true },
+            { text: 'Ártico', correct: false },
+        ]
+    },
+    {
+        question: "¿En qué año llegó el ser humano a la Luna?",
+        answers: [
+            { text: '1965', correct: false },
+            { text: '1969', correct: true },
+            { text: '1972', correct: false },
+            { text: '1959', correct: false },
+        ]
+    },
+    {
+        question: "¿Cuál es el elemento químico más abundante en el universo?",
+        answers: [
+            { text: 'Oxígeno', correct: false },
+            { text: 'Helio', correct: false },
+            { text: 'Carbono', correct: false },
+            { text: 'Hidrógeno', correct: true },
+        ]
+    },
+    {
+        question: "¿Quién pintó la Mona Lisa?",
+        answers: [
+            { text: 'Vincent van Gogh', correct: false },
+            { text: 'Leonardo da Vinci', correct: true },
+            { text: 'Pablo Picasso', correct: false },
+            { text: 'Miguel Ángel', correct: false },
+        ]
+    }
+];
