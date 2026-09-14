3134b5a095b8a5391d729437fe56331f3e92cf0f
NataliPerezG <nataliperezgarcia311@gmail.com>
2026-09-14 13:00:56 -0500

    trasting

--- src/main.js
+++ src/main.js
@@ -1,14 +1,1 @@
-const url = `https://api.api-ninjas.com/v2/randomuser?count=10`;
-
-const opciones = {
-  method: "GET",
-  headers: { "X-Api-key": "DAWYX7gM4TIy9WSI2Kudissd7MDKomGzb5PBsMMt" },
-};
-
-async function getData() {
-  const resp = await fetch(url, opciones);
-  const datos = await resp.json();
-  console.log(datos);
-}
-
-const datos = getData();
+console.log("you can, of course");
